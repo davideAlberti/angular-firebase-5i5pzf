1aa87290fecb30712f0386237617dadc8ae233b8
davideAlberti <davide.alberti@marconirovereto.it>
2019-11-11 22:03:30 +0100

    aggiunto database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from './database.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular';
+
+  constructor(private databaseService: DatabaseService) {}
+
+  ngOnInit() {
+    this.databaseService.get().then((res) => console.log(res)).catch((err) => console.log(err))
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { DatabaseService } from './database.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [ 
+    BrowserModule, 
+    FormsModule, 
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyAjyJOlrZqqQcCFZUeuTnR4GOEuBRTgJ3k",
+      authDomain: "fir-prova-3e5da.firebaseapp.com",
+      databaseURL: "https://fir-prova-3e5da.firebaseio.com",
+      projectId: "fir-prova-3e5da",
+      storageBucket: "fir-prova-3e5da.appspot.com",
+      messagingSenderId: "993087833517",
+      appId: "1:993087833517:web:dd6c345936bf3cb5bf0d24"
+    }) 
+  ],
+  declarations: [ AppComponent ],
+  bootstrap: [ AppComponent ],
+  providers: [ DatabaseService ]
 })
 export class AppModule { }
